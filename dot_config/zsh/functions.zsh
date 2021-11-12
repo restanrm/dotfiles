5446bd448783720c2353af21a1f044b85c16f828
Adrien Raffin-Caboisse <adrien.raffin@sekoia.fr>
2021-11-12 14:13:59 +0100

    Update dot_config/zsh/functions.zsh

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -68,7 +68,7 @@ function extract() {
     done
 }
 
-alias x=extract 
+alias x=extract
 
 
 archive() {
@@ -80,18 +80,18 @@ archive() {
 	echo "\t-r,--remove\tremove source file"
 	echo ""
     fi
-    if [ "$1" = "-r" ] || [ "$1" = "--remove" ]; then 
+    if [ "$1" = "-r" ] || [ "$1" = "--remove" ]; then
 	remove_source=1
 	shift
     fi
-    while (( $# > 0 )) 
+    while (( $# > 0 ))
     do
-	tar cjvf "$1".tar.bz2 "$1" 
+	tar cjvf "$1".tar.bz2 "$1"
 	success=$?
-	if [ $remove_source -eq 1 ] && [ $success -eq 0 ]; then 
+	if [ $remove_source -eq 1 ] && [ $success -eq 0 ]; then
 	    rm -rf "$1"
 	fi
-	if [ $success -ne 0 ]; then 
+	if [ $success -ne 0 ]; then
 	    echo "Erreur lors de l'opération d'archivage sur le fichier : $1"
 	    break
 	fi
@@ -100,17 +100,75 @@ archive() {
 }
 
 pause() {
-  if $(killall -SIGSTOP $1); then 
-    echo "[+] $1 successfully paused"; 
+  if $(killall -SIGSTOP $1); then
+    echo "[+] $1 successfully paused";
   else
-    echo "[-] Fail to pause $1"; 
-  fi 
+    echo "[-] Fail to pause $1";
+  fi
 }
 
 unpause() {
-  if $(killall -SIGCONT $1); then 
-    echo "[+] Successfully unpaused $1"; 
-  else 
-    echo "[-] Failed to unpause $1"; 
-  fi 
+  if $(killall -SIGCONT $1); then
+    echo "[+] Successfully unpaused $1";
+  else
+    echo "[-] Failed to unpause $1";
+  fi
 }
+
+function ssht() {
+  local out
+  first=$1
+  shift
+  for srv in "$@"; do
+    out="${out}tmux split-window 'ssh $srv';"
+  done
+  out="${out}tmux select-layout even-vertical;"
+  out="${out}ssh $first;"
+  eval $out
+}
+
+# function that infer connection to multiple instance of the same type
+# admin app-server 3
+# is equivalent to
+# ssht app-server1 app-server2 app-server3
+function admin() {
+  local server
+  local number
+  local out
+  server=$1
+  number=$2
+  for i in $(seq $number); do
+    out="${out} ${server}$i"
+  done
+  echo -ne "$out"
+  ssht ${out}
+}
+
+function vsphere() {
+  docker run --rm -it lamw/rvc rvc ara@37.187.228.30
+}
+
+# find in platform repository
+function sf() {
+  local args=$@
+  #cd $(rg /home/nrm/dev/git/github.com/SekoiaLab/platform --files --null | xargs -0 dirname | awk '!h[$0]++' |fzf -q "${args} ")
+  cd $(fd . --type d /home/nrm/dev/git/github.com/SekoiaLab/platform | fzf --query "${args} " --select-1 --exit-0)
+}
+
+# openstack context change function
+unset OS_CLOUD
+osctx () {
+  source <(gopass ls --flat | grep openrc.sh | rofi -dmenu | xargs --no-run-if-empty gopass show -n)
+}
+
+# new kctx function
+if [ -f ~/.kube.env ]; then source ~/.kube.env; fi
+kctx() {
+  local args=$1
+  conf=$HOME/.kube/configs/$(ls $HOME/.kube/configs | fzf --query "${args}" --select-1 --exit-0)
+  if [ -f "${conf}" ]; then
+    export KUBECONFIG="${conf}"
+    echo "export KUBECONFIG=${conf}" > $HOME/.kube.env
+  fi
+}
+
