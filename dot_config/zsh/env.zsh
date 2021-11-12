0c21225cee04d3089bf2deb631d983955582f378
Adrien Raffin-Caboisse <adrien.raffin@sekoia.fr>
2021-11-12 14:11:20 +0100

    Update dot_config/zsh/env.zsh

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -1,13 +1,7 @@
 # ---[ Environment ]-----------------------------------------------------
 export EDITOR='vim -p'
-#export EDITOR="emacsclient -t -a ''"
 export GOPATH=$HOME/.go
-export PATH=$PATH:$GOPATH/bin
-export PATH=/usr/local/bin:${HOME}/bin:/usr/local/sbin:/usr/sbin:/sbin:${PATH}
-#export PYTHONPATH=${HOME}/.python:${PYTHONPATH}
-#export PS_PERSONALITY='linux'
-#export GREP_OPTIONS='--color=auto --exclude-dir=\.svn --exclude-dir=\.git'
-
+#
 # Manpath & Manualpage search order
 export MANSECT=2:3:9:8:1:5:4:7:6:n
 
@@ -20,3 +14,21 @@ if [[ -r ~/.dir_colors ]]; then
 elif [[ -r /etc/DIR_COLORS ]]; then
 		eval `dircolors -b /etc/DIR_COLORS`
 fi
+
+
+# Dell xps13 related evn var
+export LIBVA_DRIVER_NAME=iHD
+
+export MOZ_ENABLE_WAYLAND=1
+export PATH=$PATH:/opt/shadow-tech/:$HOME/bin:$HOME/.cargo/bin:/usr/local/bin
+
+export BELL_ADDRESS="https://bell.restanrm.fr"
+
+# Work related env vars
+
+export VAULT_ADDR=https://vault.delivery.sekoia.io
+export PULL_REGISTRY=registry.sekoia.io
+export PUSH_REGISTRY=registry.sekoia.io
+export RELEASE_ID=latest
+export PLATFORM=dev
+
