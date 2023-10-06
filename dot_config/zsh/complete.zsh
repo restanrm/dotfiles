87e2a852c328a63d3a6de2f8af983c42a0d97bd7
Adrien Raffin-Caboisse <adrien.raffin@sekoia.io>
2023-10-06 10:16:36 +0200

    Update .config/zsh/complete.zsh

--- dot_config/zsh/complete.zsh
+++ dot_config/zsh/complete.zsh
@@ -1,39 +1,111 @@
-_sshp_commandname="sshp"
-
-_sshp_servers () {
-    local cache
-    cache="/$HOME/.cache/${_sshp_commandname}-cache"
-    if [ -e "$cache" ] && [ "$(($(date +%s) - $(/sbin/stat -c %Y "$cache")))" -lt "3600" ]; then
-        compadd ${(uo)$(cat "$cache")}
-    else
-        ssh -I /usr/lib/libykcs11.so fra1-production -- --osh groupListServers --group sre 2>/dev/null | awk -F'=' '/hostname/{print $2}' | sort -u > $cache
-        compadd ${(uo)$(cat "$cache")}
-    fi
-}
-
+# Completion for the alias sshp
 _sshp() {
     integer ret=1
-    _alternative 'servers:servers:_sshp_servers' && ret=0
+
+    # Define function only when it doesn't exist
+    (( $+functions[_sshp_cache_policy] )) || _sshp_cache_policy() {
+      # Cache invalidates after 5 minutes
+      #
+      # Reference:
+      #   https://zsh.sourceforge.net/Doc/Release/Expansion.html#index-globbing_002c-qualifiers
+      local -a oldp
+      oldp=( "$1"(ms+300) )
+      (( $#oldp ))
+    }
+
+    # Unless user explicitly turned off caching, enable caching just for this context
+    local existing_setting
+    zstyle -s ":completion:${curcontext}:" use-cache existing_setting
+    if [[ -z "${existing_setting}" ]]; then
+      zstyle ":completion:${curcontext}:" use-cache on
+    fi
+
+    # Update cache policy only when there was no existing policy
+    local existing_policy
+    zstyle -s ":completion:${curcontext}:" cache-policy existing_policy
+    if [[ -z "${existing_policy}" ]]; then
+      zstyle ":completion:${curcontext}:" cache-policy _sshp_cache_policy
+    fi
+
+    local -a instances
+    if _cache_invalid sshp_instances || ! _retrieve_cache sshp_instances; then
+      local instances
+      local stderr
+      local -i exit_code
+      () {
+        ssh -I /usr/lib/libykcs11.so fra1-production -- --osh groupListServers --group sre 2>/dev/null | awk -F'=' '/hostname/{print $2}' | sort -u >${1} 2>${2}
+        exit_code=${?}
+        IFS=$'\n\t' instances=($(<${1}))
+        stderr=$(<${2})
+      } =(:) =(:)
+
+      if (( $exit_code == 0 )); then
+        # Bastion CLI successfully executed
+        _store_cache sshp_instances instances
+      else
+        # Bastion CLI failed, abort autocompletion
+        _message -r "\
+    Failed autocomplete due to following reason:
+    ${stderr}"
+        return
+      fi
+    fi
 }
 
 compdef _sshp sshp
 
-_ssht_commandname="ssht"
-
-_ssht_servers () {
-    local cache
-    cache="/$HOME/.cache/${_ssht_commandname}-cache"
-    if [ -e "$cache" ] && [ "$(($(date +%s) - $(/sbin/stat -c %Y "$cache")))" -lt "3600" ]; then
-        compadd ${(uo)$(cat "$cache")}
-    else
-        ssh -I /usr/lib/libykcs11.so fra1-test -- --osh groupListServers --group sre 2>/dev/null | awk -F'=' '/hostname/{print $2}' | sort -u > $cache
-        compadd ${(uo)$(cat "$cache")}
-    fi
-}
-
+# Completion for the alias ssht
 _ssht() {
     integer ret=1
-    _alternative 'servers:servers:_ssht_servers' && ret=0
+
+    # Define function only when it doesn't exist
+    (( $+functions[_ssht_cache_policy] )) || _ssht_cache_policy() {
+      # Cache invalidates after 5 minutes
+      #
+      # Reference:
+      #   https://zsh.sourceforge.net/Doc/Release/Expansion.html#index-globbing_002c-qualifiers
+      local -a oldp
+      oldp=( "$1"(ms+300) )
+      (( $#oldp ))
+    }
+
+    # Unless user explicitly turned off caching, enable caching just for this context
+    local existing_setting
+    zstyle -s ":completion:${curcontext}:" use-cache existing_setting
+    if [[ -z "${existing_setting}" ]]; then
+      zstyle ":completion:${curcontext}:" use-cache on
+    fi
+
+    # Update cache policy only when there was no existing policy
+    local existing_policy
+    zstyle -s ":completion:${curcontext}:" cache-policy existing_policy
+    if [[ -z "${existing_policy}" ]]; then
+      zstyle ":completion:${curcontext}:" cache-policy _ssht_cache_policy
+    fi
+
+    local -a instances
+    if _cache_invalid ssht_instances || ! _retrieve_cache ssht_instances; then
+      local instances
+      local stderr
+      local -i exit_code
+      () {
+        ssh -I /usr/lib/libykcs11.so fra1-test -- --osh groupListServers --group sre 2>/dev/null | awk -F'=' '/hostname/{print $2}' | sort -u >${1} 2>${2}
+        exit_code=${?}
+        IFS=$'\n\t' instances=($(<${1}))
+        stderr=$(<${2})
+      } =(:) =(:)
+
+      if (( $exit_code == 0 )); then
+        # Bastion CLI successfully executed
+        _store_cache ssht_instances instances
+      else
+        # Bastion CLI failed, abort autocompletion
+        _message -r "\
+    Failed autocomplete due to following reason:
+    ${stderr}"
+        return
+      fi
+    fi
 }
 
 compdef _ssht ssht
