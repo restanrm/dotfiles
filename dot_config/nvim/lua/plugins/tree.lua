d06b569fabc4238a664296ea92d5d4867d14759b
Adrien Raffin-Caboisse <adrien.raffin@sekoia.io>
2024-06-26 18:02:00 +0200

    Update .config/nvim/lua/plugins/tree.lua

--- dot_config/nvim/lua/plugins/tree.lua
+++ dot_config/nvim/lua/plugins/tree.lua
@@ -11,7 +11,7 @@ return {
       return {
         sort_by = "case_sensitive",
         view = {
-          width = 30,
+          width = 40,
         },
         renderer = {
           group_empty = true,
