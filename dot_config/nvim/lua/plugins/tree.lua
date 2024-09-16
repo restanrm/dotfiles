205971034caee8189fdcac808b67366700f4fdc3
Adrien Raffin-Caboisse <adrien.raffin@sekoia.io>
2024-09-16 10:51:36 +0200

    Update .config/nvim/lua/plugins/tree.lua

--- dot_config/nvim/lua/plugins/tree.lua
+++ dot_config/nvim/lua/plugins/tree.lua
@@ -1,25 +1,25 @@
-return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-
-  {
-    "nvim-tree/nvim-tree.lua",
-    keys = {
-      { "<leader>tt", "<cmd>NvimTreeToggle<cr>", desc = "Toggle tree navigation" },
-      { "<leader>tf", "<cmd>NvimTreeFindFile<cr>", desc = "Find current file in tree" },
-    },
-    opts = function()
-      return {
-        sort_by = "case_sensitive",
-        view = {
-          width = 40,
-        },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = true,
-        },
-      }
-    end,
-  },
-}
+-- return {
+--   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+--
+--   {
+--     "nvim-tree/nvim-tree.lua",
+--     keys = {
+--       { "<leader>tt", "<cmd>NvimTreeToggle<cr>", desc = "Toggle tree navigation" },
+--       { "<leader>tf", "<cmd>NvimTreeFindFile<cr>", desc = "Find current file in tree" },
+--     },
+--     opts = function()
+--       return {
+--         sort_by = "case_sensitive",
+--         view = {
+--           width = 40,
+--         },
+--         renderer = {
+--           group_empty = true,
+--         },
+--         filters = {
+--           dotfiles = true,
+--         },
+--       }
+--     end,
+--   },
+-- }
