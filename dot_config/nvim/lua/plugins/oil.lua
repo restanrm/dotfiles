08edcb7ba25ef79b67b99ea18d22b62ff1f4124a
Adrien Raffin-Caboisse <raffinadrien@gmail.com>
2024-09-16 10:38:08 +0200

    Add .config/nvim/lua/plugins/oil.lua

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,17 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    { '<leader>o',
+    function()
+      require('oil').open()
+    end,
+    desc = '[F]ormat buffer', }
+  },
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+}
