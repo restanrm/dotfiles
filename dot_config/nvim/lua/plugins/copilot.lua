06c19ab115a6cd9b161142242b120ca334fbf509
Adrien Raffin-Caboisse <adrien.raffin@sekoia.io>
2025-09-29 10:56:27 +0200

    Update .config/nvim/lua/plugins/copilot.lua

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -26,21 +26,25 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
     cmd = "CopilotChat",
-    opts = function()
-      local user = vim.env.USER or "User"
-      user = user:sub(1, 1):upper() .. user:sub(2)
-      return {
-        auto_insert_mode = true,
-        question_header = "  " .. user .. " ",
-        answer_header = "  Copilot ",
-        window = {
-          width = 0.4,
-        },
-      }
-    end,
+    -- opts = function()
+    --   local user = vim.env.USER or "User"
+    --   user = user:sub(1, 1):upper() .. user:sub(2)
+    --   return {
+    --     auto_insert_mode = true,
+    --     question_header = "  " .. user .. " ",
+    --     answer_header = "  Copilot ",
+    --     window = {
+    --       width = 0.4,
+    --     },
+    --   }
+    -- end,
     keys = {
       { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
       { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+      { "<leader>ae", ":CopilotChatExplain", desc = "Explain Code" },
+      { "<leader>ac", ":CopilotChatCommit", desc = "Generate commit description" },
+      { "<leader>at", ":CopilotChatTests", desc = "Generate Tests" },
+      { "<leader>ad", ":CopilotChatDocs", desc = "Generate documentation" },
       {
         "<leader>aa",
         function()
@@ -80,18 +84,5 @@ return {
         mode = { "n", "v" },
       },
     },
-    config = function(_, opts)
-      local chat = require("CopilotChat")
-
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "copilot-chat",
-        callback = function()
-          vim.opt_local.relativenumber = false
-          vim.opt_local.number = false
-        end,
-      })
-
-      chat.setup(opts)
-    end,
   },
 }
