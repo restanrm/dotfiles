b87b0607a1947f0646959e7e7eb5efd0dc394ebf
Adrien Raffin-Caboisse <adrien.raffin@sekoia.io>
2025-11-29 15:37:10 +0100

    Update .config/nvim/lua/plugins/copilot.lua

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,27 +1,46 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "BufReadPost",
-    opts = {
-      suggestion = {
-        enabled = not vim.g.ai_cmp,
-        auto_trigger = true,
-        hide_during_completion = vim.g.ai_cmp,
-        keymap = {
-          accept = false, -- handled by nvim-cmp / blink.cmp
-          next = "<M-]>",
-          prev = "<M-[>",
-        },
-      },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
+    "github/copilot.vim",
+    enabled = false, -- copilot is disabled by default
+    keys = {
+      {
+        "<leader>ct",
+        function()
+          if vim.g.copilot_enabled == 1 then
+            vim.g.copilot_enabled = 0
+            vim.cmd("Copilot disable")
+          else
+            vim.g.copilot_enabled = 1
+            vim.cmd("Copilot enable")
+          end
+        end,
+        desc = "Toggle GitHub Copilot",
+        mode = { "n", "v" },
       },
     },
   },
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   event = "BufReadPost",
+  --   opts = {
+  --     suggestion = {
+  --       enabled = not vim.g.ai_cmp,
+  --       auto_trigger = true,
+  --       hide_during_completion = vim.g.ai_cmp,
+  --       keymap = {
+  --         accept = false, -- handled by nvim-cmp / blink.cmp
+  --         next = "<M-]>",
+  --         prev = "<M-[>",
+  --       },
+  --     },
+  --     panel = { enabled = false },
+  --     filetypes = {
+  --       markdown = true,
+  --       help = true,
+  --     },
+  --   },
+  -- },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
